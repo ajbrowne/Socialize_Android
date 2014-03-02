3742aebf8d4c825fead81afb8c864bf8d5bba709
brmahar <brmahar@mtu.edu>
2014-03-01 23:29:26 -0500

    been far

--- Socialize/src/com/lovrhatr/socialize/Login.java
+++ Socialize/src/com/lovrhatr/socialize/Login.java
@@ -2,7 +2,9 @@ package com.lovrhatr.socialize;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.TextView;
 
 public class Login extends Activity {
@@ -12,9 +14,13 @@ public class Login extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+	    getActionBar().hide();
 		setContentView(R.layout.activity_layout);
 		
 		title = (TextView)findViewById(R.id.login_title);
+		Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/mytruetypefont.ttf");
+		title.setTypeface(typeFace);
 	}
 
 	@Override
