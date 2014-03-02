8eebb3d843ed8a36ad81087ddfccdc286b10082a
Austin Browne <dazztrazak@gmail.com>
2014-03-01 22:37:05 -0500

    ssdgssgggs

--- Socialize/src/com/lovrhatr/socialize/MainActivity.java
+++ Socialize/src/com/lovrhatr/socialize/MainActivity.java
@@ -1,12 +1,21 @@
 package com.lovrhatr.socialize;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 
 public class MainActivity extends Activity {
+	
+	private LocationManager locationManager;
+	 private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +23,40 @@ public class MainActivity extends Activity {
 		Parse.initialize(this, "ZNUGpdAW35nGYe5hvleBl3IndIphZPbZjVfn8Vcn", "JGTTBGebCrZXx2J2nc6TVh3is6bwUBq5hROkFCSI");
 		setContentView(R.layout.main_lists);
 		
+		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+		boolean enabled = service
+		  .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+		// check if enabled and if not send user to the GSP settings
+		// Better solution would be to display a dialog and suggesting to 
+		// go to the settings
+		if (!enabled) {
+		  Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+		  startActivity(intent);
+		} 
+		
+		// Get the location manager
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    // Define the criteria how to select the locatioin provider -> use
+	    // default
+	    Criteria criteria = new Criteria();
+	    provider = locationManager.getBestProvider(criteria, false);
+	    Location location = locationManager.getLastKnownLocation(provider);
+	    
+	    if (location != null) {
+	        System.out.println("Provider " + provider + " has been selected.");
+	        onLocationChanged(location);
+	      } else {
+	        
+	      }
+		
 	}
+	
+	public void onLocationChanged(Location location) {
+	    int lat = (int) (location.getLatitude());
+	    int lng = (int) (location.getLongitude());
+	  }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
