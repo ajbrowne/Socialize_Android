3894600c256ae3fc36fea5367ce68709297ee327
brmahar <brmahar@mtu.edu>
2014-03-01 23:13:50 -0500

    nope

--- Socialize/gen/com/lovrhatr/socialize/R.java
+++ Socialize/gen/com/lovrhatr/socialize/R.java
@@ -21,8 +21,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int search_bg_shadow=0x7f020001;
+        public static final int custombutton=0x7f020000;
+        public static final int edit_back=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int login_button_custom=0x7f020003;
+        public static final int pressedbutton=0x7f020004;
+        public static final int search_bg_shadow=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f08000a;
